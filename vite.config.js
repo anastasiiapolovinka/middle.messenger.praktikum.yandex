e5c68adf7dfacf812791cae945cf7facc1e8883c
Anastasiia Polovynko <polovinkanasta@gmail.com>
2025-01-19 21:43:50 +0100

    chore: add config files

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import handlebars from "vite-plugin-handlebars";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-   root: resolve(__dirname, 'src'),
-   build: {
-      outDir: resolve(__dirname, 'dist'),
-   },
-   plugins: [handlebars({
-      partialDirectory: resolve(__dirname, 'src/partials'),
-      context: {}
-   })]
-})
+  root: resolve(__dirname, 'src'),
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+  },
+});
